7265287f36bbbbd075d91fbad3fa3ebe880abbea
Jonathan Nessier <jonathan.nessier@neoflow.ch>
2020-11-05 17:02:16 +0100

    Added first tests

---
+++ test/App.test.ts
@@ -0,0 +1,34 @@
+import {describe} from 'mocha';
+import {expect} from 'chai';
+import factory from '../src';
+import {JSDOM} from 'jsdom';
+import MockIntersectionObserver from './MockIntersectionObserver';
+
+describe('App test', () => {
+
+    beforeEach(() => {
+        const jsdom = new JSDOM('<!doctype html><html><head></head><body>' +
+            '<div class="sm-item">Item 1</div>' +
+            '<div class="sm-item">Item 2</div>' +
+            '</body></html>', {
+            resources: "usable",
+            runScripts: "dangerously",
+            url: "http://localhost"
+        })
+
+        globalThis.window = jsdom.window as unknown as Window & typeof globalThis;
+        globalThis.IntersectionObserver = MockIntersectionObserver;
+        globalThis.document = jsdom.window.document;
+    });
+
+    it('App is initialized', () => {
+        factory('.sm-item', {
+            initialized: (container: any, items: Array<HTMLElement>) => {
+                expect(container).to.be.a('Document');
+                expect(items).to.be.a('Array');
+                expect(Object.keys(items).length).is.equal(2);
+            }
+        });
+    });
+
+});
---
+++ test/MockIntersectionObserver.ts
@@ -0,0 +1,28 @@
+/* eslint-disable class-methods-use-this */
+
+export default class {
+    readonly root: Element | null;
+
+    readonly rootMargin: string;
+
+    readonly thresholds: ReadonlyArray<number>;
+
+    constructor() {
+        this.root = null;
+        this.rootMargin = '';
+        this.thresholds = [];
+    }
+
+    disconnect() {
+    }
+
+    observe() {
+    }
+
+    takeRecords(): IntersectionObserverEntry[] {
+        return [];
+    }
+
+    unobserve() {
+    }
+}
